eb5f086172e0e8ad5c3345818254354feb01a2d8
Manuel Rivas Gauna <manu571@gmail.com>
2021-10-28 20:36:42 -0300

    Agregado el select al navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import NavBar from './components/navbar/navbar';
-import MenuD from './components/navbar/menud';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <MenuD/>
+
     </div>
       
       
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,6 +8,7 @@ import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import BasicSelect from './select';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -51,7 +52,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-export default function SearchAppBar() {
+export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -65,13 +66,15 @@ export default function SearchAppBar() {
           >
             <MenuIcon />
           </IconButton>
+          <BasicSelect/>
+          
           <Typography
             variant="h6"
             noWrap
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
-            MUI
+            CasaFix Norte
           </Typography>
           <Search>
             <SearchIconWrapper>
@@ -82,6 +85,7 @@ export default function SearchAppBar() {
               inputProps={{ 'aria-label': 'search' }}
             />
           </Search>
+          
         </Toolbar>
       </AppBar>
     </Box>
